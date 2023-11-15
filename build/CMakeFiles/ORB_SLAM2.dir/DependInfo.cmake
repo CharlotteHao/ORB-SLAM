156834e60bec141cc06c0a72abe4d46bf0780848
shahao <1771478728@qq.com>
2023-11-15 11:30:24 +0800

    orbslam2 3

---
+++ build/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake
@@ -0,0 +1,67 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/src/Converter.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Converter.cc.o"
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/src/Frame.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Frame.cc.o"
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/src/FrameDrawer.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/FrameDrawer.cc.o"
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/src/Initializer.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Initializer.cc.o"
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/src/KeyFrame.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/KeyFrame.cc.o"
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/src/KeyFrameDatabase.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/KeyFrameDatabase.cc.o"
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/src/LocalMapping.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/LocalMapping.cc.o"
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/src/LoopClosing.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/LoopClosing.cc.o"
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/src/Map.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Map.cc.o"
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/src/MapDrawer.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/MapDrawer.cc.o"
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/src/MapPoint.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/MapPoint.cc.o"
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/src/ORBextractor.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/ORBextractor.cc.o"
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/src/ORBmatcher.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/ORBmatcher.cc.o"
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/src/Optimizer.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Optimizer.cc.o"
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/src/PnPsolver.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/PnPsolver.cc.o"
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/src/Sim3Solver.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Sim3Solver.cc.o"
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/src/System.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/System.cc.o"
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/src/Tracking.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Tracking.cc.o"
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/src/Viewer.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/src/Viewer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  "HAVE_EIGEN"
+  "HAVE_GLEW"
+  "ORB_SLAM2_EXPORTS"
+  "PANGO_DEFAULT_WIN_URI=\"wayland\""
+  "_LINUX_"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/local/include/eigen3"
+  "/usr/local/include/opencv"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_core/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_display/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_opengl/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_image/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_windowing/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/build/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_vars/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_geometry/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/tinyobj/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_glgeometry/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_packetstream/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_plot/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_scene/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_tools/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_video/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/stereo_kitti.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/Examples/Stereo/stereo_kitti.cc" "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/stereo_kitti.dir/Examples/Stereo/stereo_kitti.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  "HAVE_EIGEN"
+  "HAVE_GLEW"
+  "PANGO_DEFAULT_WIN_URI=\"wayland\""
+  "_LINUX_"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/local/include/eigen3"
+  "/usr/local/include/opencv"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_core/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_display/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_opengl/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_image/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_windowing/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/build/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_vars/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_geometry/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/tinyobj/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_glgeometry/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_packetstream/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_plot/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_scene/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_tools/include"
+  "/home/shaohao/my_study/ORBSLAM3_study/Pangolin/components/pango_video/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/shaohao/my_study/ORB-SLAM2_study/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
